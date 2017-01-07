242b5db694daf4fa55d7611ce0b07ea22010aed6
Nicola Archibald <nicola.archibald@gmail.com>
2017-01-07 23:06:24 +0000

    Initial version

---
+++ compiler.scm
@@ -0,0 +1,22 @@
+(use srfi-1)
+(use comparse)
+
+(define (na/read)
+  (read))
+
+(define (na/eval string)
+  (parse parse-expr string))
+
+(define (na/print obj)
+  (case (car obj)
+    ((number-integer)
+     (print (cadr obj)))
+    ((number-real)
+     (print (cadr obj)))
+    ((char)
+     (print (string-append "#\\" (cadr obj))))
+    ))
+
+
+                            
+    
